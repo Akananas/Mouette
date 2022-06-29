bec298dae028cbcafbed6ded28b8bbecfb57465c
oct0punk <elric.emile@gmail.com>
2022-06-29 15:39:11 +0200

    Boat

---
+++ Assets/Scripts/Boat.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour
+{
+    public int direction = 1;
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * (direction * speed * Time.deltaTime);
+        if (CheckFinish())
+            direction *= -1;
+    }
+
+    bool CheckFinish()
+    {
+        float X = Camera.main.WorldToScreenPoint(transform.position).x;
+        Debug.Log(GetComponent<SpriteRenderer>().size.x);
+        Debug.Log(X);
+        if (direction > 0)
+        {
+            return Camera.main.WorldToScreenPoint(transform.position - Vector3.right * GetComponent<SpriteRenderer>().size.x).x > Screen.width;
+        }
+        else
+        {
+            return Camera.main.WorldToScreenPoint(transform.position + Vector3.right * GetComponent<SpriteRenderer>().size.x).x < 0;
+        }
+    }
+}
