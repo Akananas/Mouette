3f17859c667b22b02ee3c4e47a083018d339667c
oct0punk <elric.emile@gmail.com>
2022-06-29 16:39:40 +0200

    Hit Boat

--- Assets/Scripts/BoatManager.cs
+++ Assets/Scripts/BoatManager.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class BoatManager : MonoBehaviour
 {
+    [SerializeField] private bool spawn = true;
     public Boat boat;
+    public Vector2 range;
 
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Spawning());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Spawn()
+    {
+        int dir = 1;
+        if (dir < 0)
+        {
+            Instantiate(boat, new Vector3(-boat.GetComponent<SpriteRenderer>().size.x, 0, 0), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(boat, new Vector3(Screen.width + boat.GetComponent<SpriteRenderer>().size.x, 0, 0), Quaternion.identity);
+        }
+    }
+
+    IEnumerator Spawning()
+    {
+        while (spawn)
+        {
+            Spawn();
+            yield return new WaitForSeconds(Random.Range(range.x, range.y));
+        }
     }
 }
