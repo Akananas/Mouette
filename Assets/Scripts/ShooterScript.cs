bcdf6917777b1f89fcd83158c85ce4163f2da39a
François Charrier <francois.charrier23@gmail.com>
2022-06-29 14:27:51 +0200

    Changement des armes

--- Assets/Scripts/ShooterScript.cs
+++ Assets/Scripts/ShooterScript.cs
@@ -1,13 +1,20 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ShooterScript : MonoBehaviour
 {
     bool m_CanShoot;
-    [SerializeField]float radius;
-    [SerializeField]LayerMask hittableObjects;
+    bool m_UseBomb;
+    [SerializeField]float m_Radius;
+    [SerializeField]float m_BombRadius;
+    [SerializeField]LayerMask m_HittableObjects;
+    Action<Vector2> m_CurrentShoot;
     
     void Start(){
         m_CanShoot = true;
+        m_UseBomb = false;
+        m_CurrentShoot = BasicShoot;
     }
     
     void Update(){
@@ -16,16 +23,36 @@ public class ShooterScript : MonoBehaviour
             Vector2 _mousePosition = Input.mousePosition;
             _mousePosition = Camera.main.ScreenToWorldPoint(_mousePosition);
             Debug.Log("Shoot");
-            BasicShoot(_mousePosition);
+            m_CurrentShoot?.Invoke(_mousePosition);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            m_UseBomb = true;
+            m_CurrentShoot = BombShoot;
         }
     }
     
     void BasicShoot(Vector2 position){
-        Collider2D[] _hitObjects = Physics2D.OverlapCircleAll(position, radius, hittableObjects);
+        Collider2D[] _hitObjects = Physics2D.OverlapCircleAll(position, m_Radius, m_HittableObjects);
         foreach (var obj in _hitObjects){
             Debug.Log("Hit object");
         }
         GameManager.Inst?.AddTimer(Reload, 0.5f);
+    }
+
+    void BombShoot(Vector2 position){
+        Collider2D[] _hitObjects = Physics2D.OverlapCircleAll(position, m_BombRadius, m_HittableObjects);
+        HashSet<Collider2D> _hitObjectsCenter = new HashSet<Collider2D>(Physics2D.OverlapCircleAll(position, m_Radius, m_HittableObjects));
+        foreach (var obj in _hitObjects){
+            if(_hitObjectsCenter.Contains(obj)){
+                Debug.Log("In center");
+            }
+            else{
+                Debug.Log("In bomb radius");
+            }
+        }
+        GameManager.Inst?.AddTimer(Reload, 0.5f);
+        m_CurrentShoot = BasicShoot;
     }
 
     void Reload(){
