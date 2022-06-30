b1e344328c9055fc195d72a6044239e5ee8dd713
François Charrier <francois.charrier23@gmail.com>
2022-06-30 11:17:26 +0200

    Chasing de l'IA qui marche mais améliorable

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -6,7 +6,6 @@ public class Boat : MonoBehaviour, IHitComp
 {
     public int direction = 1;
     [SerializeField] private float speed;
-    [SerializeField] private float health = 100.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,20 +18,20 @@ public class Boat : MonoBehaviour, IHitComp
     {
         transform.position += Vector3.right * (direction * speed * Time.deltaTime);
         if (CheckFinish())
-        {
-            ScoreManager.Instance.ScoreBoat();
-            Destroy(gameObject);
-        }
+            direction *= -1;
     }
 
-    // Return true if boat has arrived at destination
     bool CheckFinish()
     {
         float X = Camera.main.WorldToScreenPoint(transform.position).x;
         if (direction > 0)
-            return Camera.main.WorldToScreenPoint(transform.position - Vector3.right * GetComponent<SpriteRenderer>().size.x).x > Screen.width;
+        {
+            return Camera.main.WorldToScreenPoint(transform.position - Vector3.right * GetComponentInParent<SpriteRenderer>().size.x).x > Screen.width;
+        }
         else
-            return Camera.main.WorldToScreenPoint(transform.position + Vector3.right * GetComponent<SpriteRenderer>().size.x).x < 0;
+        {
+            return Camera.main.WorldToScreenPoint(transform.position + Vector3.right * GetComponentInParent<SpriteRenderer>().size.x).x < 0;
+        }
     }
     
     public float GetCurrentSpeed(){
@@ -41,8 +40,5 @@ public class Boat : MonoBehaviour, IHitComp
 
     public void Hit()
     {
-        health -= 10;
-        if (health < 0)
-            Destroy(gameObject);
     }
 }
--- Assets/Scripts/MouetteScript.cs
+++ Assets/Scripts/MouetteScript.cs
@@ -10,6 +10,7 @@ public class MouetteScript : MonoBehaviour, IHitComp
     MouetteState m_MouetteState;
     Queue<Vector2> m_Path = new Queue<Vector2>();
     [SerializeField]int m_PathStep;
+    [SerializeField]AnimationCurve m_ChasingPath;
     Vector2 m_NextPoint;
     Vector3 m_Dir;
     Transform m_MouetteTransform;
@@ -19,7 +20,7 @@ public class MouetteScript : MonoBehaviour, IHitComp
     void Start(){
         m_NextPoint = new Vector2(10, 0);
         m_MouetteTransform = transform;
-        m_Speed = 0;
+        m_Speed = 1;
         CalculateDirection();
         m_MouetteState = MouetteState.LookingForBoat;
     }
@@ -49,11 +50,10 @@ public class MouetteScript : MonoBehaviour, IHitComp
             }
             
             m_NextPoint = m_Path.Dequeue();
-            Debug.Log(m_NextPoint);
             CalculateDirection();
         }
         
-        m_MouetteTransform.position += m_Dir * m_Speed * Time.deltaTime;
+        m_MouetteTransform.position = Vector2.MoveTowards(m_MouetteTransform.position, m_NextPoint, m_Speed * Time.deltaTime);
         return true;
     }
     
@@ -67,18 +67,23 @@ public class MouetteScript : MonoBehaviour, IHitComp
         float _currentSpeed = m_Target.GetComponentInParent<Boat>().GetCurrentSpeed();
         Vector2 _targetPos = m_Target.position;
         m_Path.Clear();
+        float _endingX = _targetPos.x;
+        for(int i = 0; i <= m_PathStep; ++i){
+            _endingX += _currentSpeed / 1.5f;
+        }
         
         for(int i = 1; i <= m_PathStep; ++i){
             float _yPos = Mathf.Lerp(m_MouetteTransform.position.y, _targetPos.y, (float)i/(float)m_PathStep);
-            float _xPos = _currentSpeed * Time.deltaTime * i + _targetPos.x;
+            float _xPos = m_ChasingPath.Evaluate((float)i/(float)m_PathStep) * _currentSpeed + _endingX;
             
             m_Path.Enqueue(new Vector2(_xPos, _yPos));
         }
         
         m_NextPoint = m_Path.Dequeue();
+        m_Speed = 2;
         CalculateDirection();
+        Debug.Log(m_NextPoint);
         
-        m_Speed = Mathf.Abs(_currentSpeed);
         m_MouetteState = MouetteState.Chasing;
     }
     
@@ -97,6 +102,7 @@ public class MouetteScript : MonoBehaviour, IHitComp
         Debug.Log("Mouette hit");
         m_MouetteState = MouetteState.Hitted;
         m_Path.Clear();
-        m_NextPoint = new Vector2(m_MouetteTransform.position.x, -1);
+        m_NextPoint = new Vector2(m_MouetteTransform.position.x, -3);
+        CalculateDirection();
     }
 }
